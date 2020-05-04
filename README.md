268c2df07b005485307918ebd87e338b94982cdf
Pedro Gomez <makhnnar@gmail.com>
2020-05-03 21:44:49 -0400

    modified readme file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simplePhaserShooter
-A simple shooter in phaser.js
+A simple shooter in phaser.js made from Luis Zuno(https://www.youtube.com/user/ricosushi) tutorial series of phaser. Check this youtubelist for more info.
+
+https://www.youtube.com/playlist?list=PLCw7wwjGEim4xTHUNLqp2JDgqio4Rh78l
--- src/game.js
+++ src/game.js
@@ -1,26 +1,4 @@
-var gameSettings = {
-    playerSpeed: 200,
-    maxPowerups: 2,
-    powerUpVel: 50,
-}
-
-var config = {
-    width: 512,
-    height: 544,
-    backgroundColor: 0x000000,
-    scene:[Scene1,Scene2],
-    physics:{
-        default:"arcade",
-        acade:{
-            debug:false
-        }
-    }
-}
-
-window.onload = function(){
-    var game = new Phaser.Game(config);
-}
-
+//this is not used
 //asi se lee un json almacenado en algun lado
 /*fetch('https://raw.githubusercontent.com/makhnnar/myKotlinTips/gh-pages/manifest.json')
    .then(response => {
